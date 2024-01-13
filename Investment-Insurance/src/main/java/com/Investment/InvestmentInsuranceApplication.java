e5f5d9bfbffb2bd3f3f190c924a9def6e8cc5ca9
Ashu-029 <ashishsawale2902@gmail.com>
2024-01-13 12:50:03 +0530

    services added

---
+++ Investment-Insurance/src/main/java/com/Investment/InvestmentInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.Investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentInsuranceApplication.class, args);
+	}
+
+}
---
+++ Investment-Insurance/src/test/java/com/Investment/InvestmentInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Investment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
